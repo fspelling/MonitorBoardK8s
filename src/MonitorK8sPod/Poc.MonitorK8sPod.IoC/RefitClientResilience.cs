db576c56fb676f58a654a66de1d1d66bcd7bcbcb
Fernando Spelling <f.l.spelling@gmail.com>
2025-11-07 11:35:17 -0300

    ajuste.

--- src/MonitorK8sPod/Poc.MonitorK8sPod.IoC/RefitClientResilience.cs
+++ src/MonitorK8sPod/Poc.MonitorK8sPod.IoC/RefitClientResilience.cs
@@ -9,13 +9,52 @@ namespace Poc.MonitorK8sPod.IoC
 {
     public static class ExternalServiceResilience
     {
+        private static RetryStrategyOptions<HttpResponseMessage> retryOptions = new RetryStrategyOptions<HttpResponseMessage>()
+        {
+            BackoffType = DelayBackoffType.Exponential,
+            MaxRetryAttempts = 5,
+            UseJitter = true,
+            OnRetry = static args =>
+            {
+                return new ValueTask(Task.Run(() => Console.WriteLine($"[Webhook Retry] Tentativa #{args.AttemptNumber} - Erro: {args.Outcome.Exception?.Message}")));
+            },
+            ShouldHandle = static args =>
+            {
+                return ValueTask.FromResult(args.Outcome switch
+                {
+                    { Result.StatusCode: HttpStatusCode.TooManyRequests } => true,
+                    { Result.StatusCode: HttpStatusCode.RequestTimeout } => true,
+                    { Result.StatusCode: HttpStatusCode.InternalServerError } => true,
+                    { Exception: not null } => true,
+                    _ => false
+                });
+            }
+        };
+
+        private static CircuitBreakerStrategyOptions<HttpResponseMessage> circuitOptions = new CircuitBreakerStrategyOptions<HttpResponseMessage>()
+        {
+            BreakDuration = TimeSpan.FromSeconds(10),
+            MinimumThroughput = 5,
+            FailureRatio = 0.2,
+            ShouldHandle = static args =>
+            {
+                return ValueTask.FromResult(args.Outcome switch
+                {
+                    { Result.StatusCode: HttpStatusCode.TooManyRequests } => true,
+                    { Result.StatusCode: HttpStatusCode.RequestTimeout } => true,
+                    { Result.StatusCode: HttpStatusCode.InternalServerError } => true,
+                    _ => false
+                });
+            }
+        };
+
         public static IServiceCollection AddHttpClientWithResilience(this IServiceCollection services, string httpClientName)
         {
             services.AddHttpClient(httpClientName)
                     .AddResilienceHandler("HttpClientPipeline", builder =>
                     {
-                        builder.AddRetry(SetConfigRetry());
-                        builder.AddCircuitBreaker(SetConfigCircuitBreaker());
+                        builder.AddRetry(retryOptions);
+                        builder.AddCircuitBreaker(circuitOptions);
                     });
 
             return services;
@@ -28,56 +67,11 @@ namespace Poc.MonitorK8sPod.IoC
                     .ConfigureHttpClient(c => c.BaseAddress = new Uri(baseUrl))
                     .AddResilienceHandler("RefitPipeline", builder =>
                     {
-                        builder.AddRetry(SetConfigRetry());
-                        builder.AddCircuitBreaker(SetConfigCircuitBreaker());
+                        builder.AddRetry(retryOptions);
+                        builder.AddCircuitBreaker(circuitOptions);
                     });
 
             return services;
-        }
-
-        private static RetryStrategyOptions<HttpResponseMessage> SetConfigRetry()
-        {
-            return new RetryStrategyOptions<HttpResponseMessage>
-            {
-                BackoffType = DelayBackoffType.Exponential,
-                MaxRetryAttempts = 5,
-                UseJitter = true,
-                OnRetry = static args =>
-                {
-                    return new ValueTask(Task.Run(() => Console.WriteLine($"[Webhook Retry] Tentativa #{args.AttemptNumber} - Erro: {args.Outcome.Exception?.Message}")));
-                },
-                ShouldHandle = static args =>
-                {
-                    return ValueTask.FromResult(args.Outcome switch
-                    {
-                        { Result.StatusCode: HttpStatusCode.TooManyRequests } => true,
-                        { Result.StatusCode: HttpStatusCode.RequestTimeout } => true,
-                        { Result.StatusCode: HttpStatusCode.InternalServerError } => true,
-                        { Exception: not null } => true,
-                        _ => false
-                    });
-                }
-            };
-        }
-
-        private static CircuitBreakerStrategyOptions<HttpResponseMessage> SetConfigCircuitBreaker()
-        {
-            return new CircuitBreakerStrategyOptions<HttpResponseMessage>()
-            {
-                BreakDuration = TimeSpan.FromSeconds(10),
-                MinimumThroughput = 5,
-                FailureRatio = 0.2,
-                ShouldHandle = static args =>
-                {
-                    return ValueTask.FromResult(args.Outcome switch
-                    {
-                        { Result.StatusCode: HttpStatusCode.TooManyRequests } => true,
-                        { Result.StatusCode: HttpStatusCode.RequestTimeout } => true,
-                        { Result.StatusCode: HttpStatusCode.InternalServerError } => true,
-                        _ => false
-                    });
-                }
-            };
         }
     }
 }
