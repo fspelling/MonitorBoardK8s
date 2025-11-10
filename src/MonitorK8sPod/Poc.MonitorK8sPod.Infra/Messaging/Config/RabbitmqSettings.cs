c9ae1549848aaebfa016609b3c13d47d7ad1f65b
Fernando Spelling <f.l.spelling@gmail.com>
2025-11-10 11:11:18 -0300

    ajuste

--- src/MonitorK8sPod/Poc.MonitorK8sPod.Infra/Messaging/Config/RabbitmqSettings.cs
+++ src/MonitorK8sPod/Poc.MonitorK8sPod.Infra/Messaging/Config/RabbitmqSettings.cs
@@ -5,6 +5,6 @@
         public string HostName { get; init; } = string.Empty;
         public string UserName { get; init; } = string.Empty;
         public string Password { get; init; } = string.Empty;
-        public string Port { get; init; } = string.Empty;
+        public int Port { get; init; } = 0;
     }
 }
